943444eb7e8930a5e9ee6f65b9b00ab3a6dbcd14
kirwa-KO <56740152+kirwa-KO@users.noreply.github.com>
2020-02-07 23:26:51 +0100

    hamdolah

---
+++ remove_cashe.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+const=0.3
+figlet Service Thala
+cd	&& rm -rf .*42*
+cd ~/Library && rm -rf *42*
+echo '#........................!(8%)\r'
+sleep $const
+echo '###......................!(16%)\r'
+sleep $const
+echo '#####....................!(24%)\r'
+sleep $const
+echo '#######..................!(32%)\r'
+sleep $const
+echo '#########................!(40%)\r'
+sleep $const
+echo '###########..............!(48%)\r'
+sleep $const
+echo '#############............!(56%)\r'
+sleep $const
+echo '###############..........!(64%)\r'
+sleep $const
+echo '#################........!(72%)\r'
+sleep $const
+echo '###################......!(80%)\r'
+sleep $const
+echo '#####################....!(88%)\r'
+sleep $const
+echo '#######################..!(100%)\r'
+figlet Thala .. !
+cd ~/Desktop
